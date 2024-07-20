a72119f42d405e73cd6210d95b9e3bb2e31be931
ahmed <scilton19@gmial.com>
2024-07-20 17:57:22 +0300

    initialize dio

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:plants_informations/core/api/dio_helper.dart';
 
 import 'app.dart';
 import 'core/constants/my_bloc_observer.dart';
@@ -7,6 +9,6 @@ import 'core/constants/my_bloc_observer.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
-
+  await DioHelper.init();
   runApp(const MyApp());
 }
