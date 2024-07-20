64c85d5304357b7c630ededf99e32b6bd1618ffe
ahmed <scilton19@gmial.com>
2024-07-20 17:44:30 +0300

    Add end points

---
+++ lib/core/utils/end_points.dart
@@ -0,0 +1,8 @@
+class EndPoints {
+  /// Base URL
+  static const String baseURL = "https://trefle.io";
+
+  /// API Key
+  static const String allPlants = "/api/v1/plants";
+  static const String zonePlants = "/api/v1/distributions/";
+}
