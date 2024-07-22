b4f876bc7e377897c9b3cbf777f2a9d46d86aa75
ahmed <scilton19@gmial.com>
2024-07-22 11:52:39 +0300

    Change app icon and name

--- lib/view/widgets/filter_widget.dart
+++ lib/view/widgets/filter_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:plants_informations/control/home_control/home_cubit.dart';
+import 'package:plants_informations/core/utils/app_colors.dart';
 
 import '../../core/utils/app_text_styles.dart';
 
@@ -12,9 +13,7 @@ class FilterWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     HomeCubit controller = context.read<HomeCubit>();
     return BlocConsumer<HomeCubit, HomeState>(
-      listener: (context, state) {
-        // TODO: implement listener
-      },
+      listener: (context, state) {},
       builder: (context, state) {
         return SizedBox(
           height: 60.h,
@@ -22,24 +21,24 @@ class FilterWidget extends StatelessWidget {
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) => Padding(
               padding: EdgeInsets.symmetric(horizontal: 5.w),
-              child: InkWell(
-                onTap: () {
-                  controller.changeFilter(index);
-                },
-                child: Card(
-                  elevation: 2,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(20),
-                      bottomLeft: Radius.circular(20),
-                    ),
+              child: Card(
+                elevation: 2,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
                   ),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    controller.changeFilter(index);
+                  },
                   child: Container(
                     alignment: Alignment.center,
                     decoration: BoxDecoration(
                       color: controller.filterSelectedIndex == index
-                          ? Colors.blue
-                          : Colors.grey,
+                          ? AppColors.selectedFilter
+                          : AppColors.notSelectedFilter,
                       borderRadius: const BorderRadius.only(
                         topRight: Radius.circular(20),
                         bottomLeft: Radius.circular(20),
